90551c5281df61303c2beb2af7e81c2f9585b708
unknown <hMens@ITEM-59750.dhcp.mtrl.fr.sopra>
2013-07-16 08:30:58 +0100

    Début de l'écran statistiques

--- application/controllers/controller.php
+++ application/controllers/controller.php
@@ -9,6 +9,14 @@ class Controller extends CI_Controller {
 	
 	public function stats()
 	{
-		$this->twiggy->template('stats')->display();
+        $data = array(
+                    'time_spent' => '0h 0m 0s',
+                    'time_left' => '0h 0m 0s',
+                    'over_time' => '0h 0m 0s',
+                    'over_time_today' => '0h 0m 0s',
+                    'end_time' => '0h 0m 0s',
+                    'end_time_no_over_time' => '0h 0m 0s'
+                );
+        $this->twiggy->set($data, NULL)->template('stats')->display();
 	} 
 }
