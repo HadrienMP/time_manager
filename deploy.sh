7ad810ddf200a201aeed400b27e166cacfce440b
HadrienMP <h.menspellen@gmail.com>
2013-07-19 14:20:41 +0100

    Amélioration du script de déploiement

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@
 find . -iname "*.php" | xargs chmod 755
 path=${PWD}
 python deploy.py $path
+echo ""
+echo "!!!ATTENTION!!!"
+echo "Ne pas oublier de modifier les paramètres tank_auth pour correspondre au serveur et au mode"
