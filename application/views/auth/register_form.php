5252666bbc4cc121551553770e7f49e59f68323f
HadrienMP <h.menspellen@gmail.com>
2013-07-19 15:38:04 +0100

    Amélioration de l'affichage des erreurs du formulaire d'enregistrement

--- application/views/auth/register_form.php
+++ application/views/auth/register_form.php
@@ -39,26 +39,34 @@ $captcha = array(
 <div class="form-wrapper">
 <table>
 	<?php if ($use_username) { ?>
+    <tr>
+        <td class="error" colspan="2"><?php echo form_error($username['name']); ?><?php echo isset($errors[$username['name']])?$errors[$username['name']]:''; ?></td>
+    </tr>
 	<tr>
 		<td><?php echo form_label('Username', $username['id']); ?></td>
 		<td><?php echo form_input($username); ?></td>
-		<td style="color: red;"><?php echo form_error($username['name']); ?><?php echo isset($errors[$username['name']])?$errors[$username['name']]:''; ?></td>
 	</tr>
 	<?php } ?>
-	<tr>
+    <tr>
+		<td class="error" colspan="2"><?php echo form_error($email['name']); ?><?php echo isset($errors[$email['name']])?$errors[$email['name']]:''; ?></td>
+	</tr>
+    <tr>
 		<td><?php echo form_label('Email Address', $email['id']); ?></td>
 		<td><?php echo form_input($email); ?></td>
-		<td style="color: red;"><?php echo form_error($email['name']); ?><?php echo isset($errors[$email['name']])?$errors[$email['name']]:''; ?></td>
+	</tr>
+	<tr>
+		<td class="error" colspan="2"><?php echo form_error($password['name']); ?></td>
 	</tr>
 	<tr>
 		<td><?php echo form_label('Password', $password['id']); ?></td>
 		<td><?php echo form_password($password); ?></td>
-		<td style="color: red;"><?php echo form_error($password['name']); ?></td>
+	</tr>
+	<tr>
+		<td class="error" colspan="2"><?php echo form_error($confirm_password['name']); ?></td>
 	</tr>
 	<tr>
 		<td><?php echo form_label('Confirm Password', $confirm_password['id']); ?></td>
 		<td><?php echo form_password($confirm_password); ?></td>
-		<td style="color: red;"><?php echo form_error($confirm_password['name']); ?></td>
 	</tr>
 
 	<?php if ($captcha_registration) {
@@ -90,9 +98,11 @@ $captcha = array(
 		</td>
 	</tr>
 	<tr>
+		<td class="error" colspan="2"><?php echo form_error($captcha['name']); ?></td>
+	</tr>
+	<tr>
 		<td><?php echo form_label('Confirmation Code', $captcha['id']); ?></td>
 		<td><?php echo form_input($captcha); ?></td>
-		<td style="color: red;"><?php echo form_error($captcha['name']); ?></td>
 	</tr>
 	<?php }
 	} ?>
