0380a9dc1e7e3fd3f49dc137647c70023f394aaa
HadrienMP <h.menspellen@gmail.com>
2013-08-17 15:32:23 +0200

    Relativisation du fichier index.php

--- deploy.py
+++ deploy.py
@@ -17,24 +17,6 @@ path = path[0].upper() + path[1:]
 print "    - Chemin modifié : " + path
 
 # Lecture du fichier
-print "\nOuverture du fichier htdocs/index.php pour modification"
-path_file = 'htdocs/index.php'
-f = open(path_file,'r')
-index_php = f.read()
-f.close()
-
-index_php = re.sub(r"(application_folder|system_path) = '.*(application|system)'", r"\1 = '" + path + r"/\2'", index_php)
-
-# Réécriture dans le fichier
-f = open(path_file,'w')
-f.write(index_php)
-f.close()
-
-print "Fin de la modification du fichier htdocs/index.php"
-
-
-
-# Lecture du fichier
 print "\nOuverture du fichier conf/time_manager.conf pour modification"
 path_conf_file = 'conf/time_manager.conf'
 f = open(path_conf_file,'r')
