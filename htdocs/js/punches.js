3b5ca88298933d202ae3ee298f66c172ac60f32a
HadrienMP <h.menspellen@gmail.com>
2013-11-08 15:21:14 +0000

    UI Improvements to the punches screen

--- htdocs/js/punches.js
+++ htdocs/js/punches.js
@@ -3,11 +3,15 @@ $(function() {
 	$('.checks').last().show();
     
     $('a#previous').click(function() {
-        $('.checks:visible').hide().prev().show();
+        if ($('.checks').index($('.checks:visible')) != 0) {
+            $('.checks:visible').hide().prev().show();
+        }
         return false;
     });
     $('a#next').click(function() {
-        $('.checks:visible').hide().next().show();
+        if ($('.checks').index($('.checks:visible')) != $('.checks').length -1) {
+            $('.checks:visible').hide().next().show();
+        }
         return false;
     });
 });
