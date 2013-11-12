b54a16fe055aef8dfdd71d08734847b98513d9d1
HadrienMP <h.menspellen@gmail.com>
2013-11-12 19:35:13 +0100

    Adds the custom form validation library

---
+++ application/libraries/MY_Form_validation.php
@@ -0,0 +1,69 @@
+<?php
+class MY_Form_validation extends CI_Form_validation
+{
+     function __construct($config = array())
+     {
+          parent::__construct($config);
+     }
+     
+	/**
+	 * Error String
+	 *
+	 * Returns the error messages as a string, wrapped in the error delimiters
+	 *
+	 * @access	public
+	 * @param	string
+	 * @param	string
+	 * @return	str
+	 */
+	public function error_string($prefix = '', $suffix = '')
+	{
+		$this->_error_array = array_unique($this->_error_array);
+		
+		// No errrors, validation passes!
+		if (count($this->_error_array) === 0)
+		{
+			return '';
+		}
+
+		if ($prefix == '')
+		{
+			$prefix = $this->_error_prefix;
+		}
+
+		if ($suffix == '')
+		{
+			$suffix = $this->_error_suffix;
+		}
+
+		// Generate the error string
+		$str = '';
+		foreach ($this->_error_array as $val)
+		{
+			if ($val != '')
+			{
+				$str .= $prefix.$val.$suffix."\n";
+			}
+		}
+
+		return $str;
+	}
+ 
+    /**
+     * Error Array
+     *
+     * Returns the error messages as an array
+     *
+     * @return  array
+     */
+    function error_array()
+    {
+        if (count($this->_error_array) === 0)
+        {
+                return FALSE;
+        }
+        else
+            return $this->_error_array;
+ 
+    }
+}
