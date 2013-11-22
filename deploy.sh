7b18914268e54bcca77699d8ca8b5b545edbd0c6
HadrienMP <public@hadrienmp.fr>
2013-11-22 18:37:52 +0100

    Deletes useless content

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-find . -iname "*.php" | xargs chmod 755
-path=${PWD}
-python deploy.py $path
-echo ""
-echo "!!!ATTENTION!!!"
-echo "Ne pas oublier de modifier les paramètres tank_auth pour correspondre au serveur et au mode"
