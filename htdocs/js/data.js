bcfc62c2695575585471c2ca1d4b7c0cf98e69e7
HadrienMP <public@hadrienmp.fr>
2013-12-06 18:24:09 +0100

    Adds a confirmation dialog upon data export

--- htdocs/js/data.js
+++ htdocs/js/data.js
@@ -4,4 +4,31 @@ $(function() {
 		value : parseInt($(".progress-label").text()),
 	});
 	
+    $( "#dialog-confirm" ).dialog({
+    	autoOpen: false,
+    	width: 500,
+		resizable: false,
+		modal: true
+    });
+    
+    $('#export').click(function(event) {
+    	event.preventDefault();
+    	var targetUrl = $(this).attr("href");
+
+        $( "#dialog-confirm" ).dialog({
+	    	buttons: {
+				"Exporter les pointages": function() {
+					$( this ).dialog( "close" );
+					window.open(targetUrl,'mywindow','width=400,height=200,toolbar=yes, \
+							location=yes,directories=yes,status=yes,menubar=yes, \
+							scrollbars=yes,copyhistory=yes,resizable=yes');
+				},
+				Cancel: function() {
+					$( this ).dialog( "close" );
+					return false;
+				}
+			}
+        });
+    	$( "#dialog-confirm" ).dialog( "open" );
+    });
 });
